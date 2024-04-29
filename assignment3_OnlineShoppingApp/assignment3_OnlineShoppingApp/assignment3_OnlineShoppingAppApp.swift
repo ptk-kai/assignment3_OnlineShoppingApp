610431ee60f47b0c542109f411561df52cdf3d5c
tuankiet_phan <102800149+ptk-kai@users.noreply.github.com>
2024-04-30 04:38:13 +1000

    Initial commit

---
+++ assignment3_OnlineShoppingApp/assignment3_OnlineShoppingApp/assignment3_OnlineShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignment3_OnlineShoppingAppApp.swift
+//  assignment3_OnlineShoppingApp
+//
+//  Created by Tuan Kiet Phan on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct assignment3_OnlineShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
